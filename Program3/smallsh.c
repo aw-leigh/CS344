af899cf5f1f71081b45a67280688d9105b17ebfb
Andrew.OSUServer <wilsoan6@oregonstate.edu>
2019-08-02 13:04:42 -0700

    added bakcground commands

--- Program3/smallsh.c
+++ Program3/smallsh.c
@@ -10,6 +10,57 @@
 #include <stdbool.h> //for bools
 #include <pwd.h>     //for finding HOME directory
 
+
+void printExitStatus(int * status, bool * signalFlag){
+    char *exitStatusMessage;
+    int exitStatusSize;
+
+    if (*signalFlag == false)
+    {
+        exitStatusMessage = "exit value ";
+        exitStatusSize = 11;
+    }
+    else
+    {
+        exitStatusMessage = "terminated by signal ";
+        exitStatusSize = 21;
+    }
+    int statusHex = 0x30; // 0 in hex, refernece: https://stackoverflow.com/a/9597106
+    statusHex += *status; // add the last exit status number to the hex value
+    char buff[] = {1, statusHex, '\n'};
+    write(STDOUT_FILENO, exitStatusMessage, exitStatusSize);
+    fflush(stdout);
+    write(STDOUT_FILENO, buff, sizeof(buff));
+    fflush(stdout);
+}
+
+void findZombies(int * status, bool * signalFlag){
+
+    pid_t pid = NULL;
+    int childExitMethod = -1337;
+
+    //while there are any finished processes hanging around
+    while ((pid = waitpid(-1, &childExitMethod, WNOHANG)) > 0){
+        
+        //return exit status value
+        if (WIFEXITED(childExitMethod) != 0)
+        {
+            *signalFlag = false;
+            *status = WEXITSTATUS(childExitMethod);
+        }
+        //return terminating signal value
+        else if (WIFSIGNALED(childExitMethod) != 0)
+        {
+            *signalFlag = true;
+            *status = WTERMSIG(childExitMethod);
+        }
+        
+        printf("Background pid %d is done: ", pid);
+        fflush(stdout);
+        printExitStatus(status, signalFlag);   
+    }
+}
+
 void findRedirections(char *userInputArray[], int numArguments, int *inPipeLocation, int *outPipeLocation)
 {
     for (int i = 0; i < numArguments; i++)
@@ -26,7 +77,7 @@ void findRedirections(char *userInputArray[], int numArguments, int *inPipeLocat
 }
 
 //returns -8888 if file could not be opened, otherwise returns file descriptor
-int openFileForReading(char *userInputArray[], int *numArguments, int * inFileDescriptor, int inPipeLocation)
+int openFileForReading(char *userInputArray[], int *numArguments, int *inFileDescriptor, int inPipeLocation)
 {
 
     //grab file path from the token following < and open file
@@ -36,7 +87,7 @@ int openFileForReading(char *userInputArray[], int *numArguments, int * inFileDe
     if (*inFileDescriptor < 0)
     {
         perror("Unable to open input file\n");
-		return -8888;          
+        return -8888;
     }
     //otherwise, remove < symbol and filepath from array,
     //shifting everything over 2 places
@@ -59,7 +110,7 @@ int openFileForWriting(char *userInputArray[], int *numArguments, int *outFileDe
     if (*outFileDescriptor < 0)
     {
         perror("Unable to open output file\n");
-		return -8888;        
+        return -8888;
     }
     //otherwise, remove '>' symbol and filepath from array,
     //shifting everything over 2 places
@@ -73,16 +124,86 @@ int openFileForWriting(char *userInputArray[], int *numArguments, int *outFileDe
 
 void startBackgroundProcess(char *userInputArray[], int numArguments)
 {
-}
-
-void startForegroudProcess(char *userInputArray[], int numArguments, int *status, bool *signalFlag)
-{
-    pid_t pid = fork();
     int childExitMethod = -1337;
     int inPipeLocation = -1337;
     int outPipeLocation = -1337;
     int inFileDescriptor = -1337;
     int outFileDescriptor = -1337;
+
+    pid_t pid = fork();
+
+    switch (pid)
+    {
+    case (-1):
+        perror("could not fork!");
+        exit(1);
+        break;
+    case (0): //child
+        //input redirection with dup2()
+        findRedirections(userInputArray, numArguments, &inPipeLocation, &outPipeLocation);
+        if (inPipeLocation != -1337) //if we found a ">"
+        {
+            if (openFileForReading(userInputArray, &numArguments, &inFileDescriptor, inPipeLocation) == -8888)
+            { //if could not open file, error and exit
+                printf("could not open file for reading\n");
+                exit(1);
+            }
+            dup2(3, 0); //if opened, redirect stdin
+        }
+        else //if there was no redirection for reading, redirect to dev/null
+        {
+            inFileDescriptor = open("/dev/null", O_RDONLY);
+            dup2(inFileDescriptor, 0);
+        }
+        if (outPipeLocation != -1337)
+        {
+            if (openFileForWriting(userInputArray, &numArguments, &outFileDescriptor, outPipeLocation) == -8888)
+            { //if could not open file, error and exit
+                printf("could not open file for writing\n");
+                exit(1);
+            }
+            if (inPipeLocation != -1337) //if we found a "<"
+            {
+                dup2(4, 1); //files open for both reading and writing
+            }
+            else
+            {
+                dup2(3, 1); //only open for writing
+            }
+        }
+        else //if there was no redirection for writing, redirect to dev/null
+        {
+            outFileDescriptor = open("/dev/null", O_RDONLY);
+            dup2(outFileDescriptor, 0);
+        }
+        //exec()
+        userInputArray[numArguments] = NULL;
+        userInputArray[numArguments - 1] = NULL; //remove trailing ampersand
+        if (execvp(userInputArray[0], userInputArray) == -1)
+        {
+            printf("command \"%s\" is not a valid command\n", userInputArray[0]);
+            fflush(stdout);
+            return;
+        }
+        close(inFileDescriptor);
+        close(outFileDescriptor);
+        break;
+    }
+    waitpid(pid, &childExitMethod, WNOHANG);
+    printf("background pid is %d\n", pid);
+    fflush(stdout);
+}
+
+void startForegroudProcess(char *userInputArray[], int numArguments, int *status, bool *signalFlag)
+{
+
+    int childExitMethod = -1337;
+    int inPipeLocation = -1337;
+    int outPipeLocation = -1337;
+    int inFileDescriptor = -1337;
+    int outFileDescriptor = -1337;
+
+    pid_t pid = fork();
 
     switch (pid)
     {
@@ -122,7 +243,8 @@ void startForegroudProcess(char *userInputArray[], int numArguments, int *status
         }
         //exec()
         userInputArray[numArguments] = NULL;
-        if(execvp(userInputArray[0], userInputArray) == -1){
+        if (execvp(userInputArray[0], userInputArray) == -1)
+        {
             *status = 1;
             *signalFlag = false;
             printf("command \"%s\" is not a valid command\n", userInputArray[0]);
@@ -176,26 +298,7 @@ bool executeInput(char *userInputArray[], int numArguments, int *status, bool *s
     /*** STATUS ***/
     else if (strcmp(userInputArray[0], "status") == 0)
     {
-        char *exitStatusMessage;
-        int exitStatusSize;
-
-        if (*signalFlag == false)
-        {
-            exitStatusMessage = "exit value ";
-            exitStatusSize = 11;
-        }
-        else
-        {
-            exitStatusMessage = "terminated by signal ";
-            exitStatusSize = 21;
-        }
-        int statusHex = 0x30; // 0 in hex, refernece: https://stackoverflow.com/a/9597106
-        statusHex += *status; // add the last exit status number to the hex value
-        char buff[] = {1, statusHex, '\n'};
-        write(STDOUT_FILENO, exitStatusMessage, exitStatusSize);
-        fflush(stdout);
-        write(STDOUT_FILENO, buff, sizeof(buff));
-        fflush(stdout);
+        printExitStatus(status, signalFlag);
     }
 
     /*** OTHER COMMAND ***/
@@ -210,6 +313,8 @@ bool executeInput(char *userInputArray[], int numArguments, int *status, bool *s
             startForegroudProcess(userInputArray, numArguments, status, signalFlag);
         }
     }
+
+    findZombies(status, signalFlag);
     return false;
 }
 
@@ -286,7 +391,7 @@ void mainShellLoop()
 
 int main(int argc, char **argv)
 {
-    
+
     // Run command loop.
     mainShellLoop();
 
